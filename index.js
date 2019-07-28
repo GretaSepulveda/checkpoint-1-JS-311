a22146fce1e3175d32d76405bddf22877cd9a6ad
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2019-07-28 18:23:40 -0500

    updated project checkpoint 1

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+const bodyParser = require('body-parser')
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => res.send('default route'))
 
